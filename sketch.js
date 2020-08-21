4d9cd4d7776bb7780ac0ff4b17c785f6900bfb4a
BSSanjai <69799493+BSSanjai@users.noreply.github.com>
2020-08-21 17:59:45 +0530

    Update sketch.js
    added bounceOff function for edges and sprites.

--- sketch.js
+++ sketch.js
@@ -1,32 +1,58 @@
-var sprite, sprite1 ,sprite2;
+var sprite, sprite1 ,sprite2, sprite3, sprite4, topEdge, bottomEdge, leftEdge, rightEdge;
 
 function setup() {
-  createCanvas(1000,1000);
-  sprite=createSprite(400, 200, 50, 50);
-  sprite1=createSprite(460, 300, 40, 60);
-  sprite2=createSprite(160, 400, 30, 30);
+  createCanvas(400,400);
+  sprite=createSprite(380, 20, 20, 20);
+  sprite1=createSprite(20, 380, 20, 20);
+  sprite2=createSprite(160, 300, 30, 30);
+  sprite3=createSprite(320, 40, 31, 40);
+  sprite4=createSprite(260, 340, 39, 38);
+  
+  topEdge=createSprite(200, 0, 400, 5);
+  bottomEdge=createSprite(200, 400, 400, 5);
+  leftEdge=createSprite(0, 200, 5, 400);
+  rightEdge=createSprite(400, 200, 5, 400);
+
+
+  sprite.velocityX=-2;
+  sprite.velocityY=2;
+  sprite1.velocityX=2;
+  sprite1.velocityY=-2;
+  sprite2.velocityX=2;
+  sprite2.velocityY=-2;
+  sprite3.velocityX=2;
+  sprite3.velocityY=-2;
+  sprite4.velocityX=2;
+  sprite4.velocityY=-2;
 }
 
 function draw() {
  background(0,0,0);  
 sprite.shapeColor="red";
-sprite1.x=mouseX;
-sprite1.y=mouseY;
+sprite1.shapeColor="yellow";
+sprite2.shapeColor="green";
+sprite3.shapeColor="blue";
+sprite4.shapeColor="pink";
+
+
+
 isTouching(sprite,sprite1);
-isTouching(sprite1,sprite2);
+isTouching(sprite2,sprite1);
+isTouching(sprite3,sprite1);
+isTouching(sprite4,sprite1);
 
+bounceOffEdges(sprite1, topEdge, bottomEdge,leftEdge, rightEdge )
+bounceOffEdges(sprite, topEdge, bottomEdge,leftEdge, rightEdge )
+bounceOffEdges(sprite2, topEdge, bottomEdge,leftEdge, rightEdge )
+bounceOffEdges(sprite3, topEdge, bottomEdge,leftEdge, rightEdge )
+bounceOffEdges(sprite4, topEdge, bottomEdge,leftEdge, rightEdge )
 
+bounceOff(sprite,sprite1)
 // if(sprite1.isTouching(sprite)){
 // sprite.bounce;
 // }
   drawSprites();
 }
-function isTouching(sprite,sprite1) {
- if( Math.abs(sprite.y-sprite1.y)<sprite.height/2+sprite1.height/2  &&  Math.abs(sprite.x-sprite1.x)<sprite.width/2+sprite1.width/2){
-// sprite.shapeColor=rgb(random(0,255),220,255);
-// sprite1.shapeColor=rgb(255,210,random(30,290));
-sprite.shapeColor="Green";
-sprite1.shapeColor="Green";
- }
- 
-}
+
+
+
